3b63e777712115547d4fc01285c976f2b9a4d0ba
Varshant Kumar <vkumar@alpha-sense.com>
2023-08-19 16:19:17 +0530

    fix: tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {buildSubgraphSchema} from "@apollo/subgraph";
-import {ApolloServer, ContextFunction} from "@apollo/server";
+import {ApolloServer} from "@apollo/server";
 import {
   startStandaloneServer,
 } from "@apollo/server/standalone";
